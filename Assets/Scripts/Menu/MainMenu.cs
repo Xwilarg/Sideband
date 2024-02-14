76d5ae9f8d4f04f53ec28ca9157b494742aadaca
Xwilarg <xwilarg@protonmail.com>
2024-02-14 14:01:38 +0100

    Add main menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RhythmJam2024
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _creditsPanel;
+
+        public void PlayAlone()
+        {
+            StaticData.IsAgainstAI = true;
+            SceneManager.LoadScene("LevelSelector");
+        }
+
+        public void Play2P()
+        {
+            StaticData.IsAgainstAI = false;
+            SceneManager.LoadScene("LevelSelector");
+        }
+
+        public void OpenCredits()
+        {
+            _creditsPanel.SetActive(true);
+        }
+
+        public void CloseCredits()
+        {
+            _creditsPanel.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/StaticData.cs
+++ Assets/Scripts/StaticData.cs
@@ -2,7 +2,7 @@
 {
     public static class StaticData
     {
-        public static bool IsAgainstAI => true;
+        public static bool IsAgainstAI { set; get; }
         public static int SongSelected { set; get; }
     }
 }
