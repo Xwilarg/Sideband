995f69263634fa7f20b178966fe3828a5c43037b
Xwilarg <xwilarg@protonmail.com>
2024-02-18 12:41:00 +0100

    Revert changes

--- Assets/RhythmEngine/Samples/ManiaDemo/Common/Scripts/SimpleManiaSong.cs
+++ Assets/RhythmEngine/Samples/ManiaDemo/Common/Scripts/SimpleManiaSong.cs
@@ -6,9 +6,9 @@ namespace RhythmEngine.Examples
     /// <summary>
     /// This version of a Song contains all the data needed to play the mania demo.
     /// </summary>
-    //[CreateAssetMenu(fileName = "SimpleManiaSong", menuName = "RhythmEngine/Songs/Examples/SimpleManiaSong")]
+    [CreateAssetMenu(fileName = "SimpleManiaSong", menuName = "RhythmEngine/Songs/Examples/SimpleManiaSong")]
     // Feel free to uncomment the above line if you want to create more mania song assets.
-    public class SimpleManiaSong : BeatSequencedSong
+    public class SimpleManiaSong : ScriptableObject
     {
         [Header("Mania Notes")]
         public List<SimpleManiaNote> Notes = new();
